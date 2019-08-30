a786cbebb32de2b6e82c0bc5434752f75c545e24
rafaelaltamirano <rafaelaltamirano_@outlook.com>
2019-08-30 13:20:17 -0300

    segunda version

---
+++ Deposito/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Deposito
+{
+    class Usuario
+    {
+        [DisplayName("idUsuario")]
+        public string idusuario { get; set; }
+        [DisplayName("password")]
+        private string pass { get; set; }
+        [DisplayName("Email")]
+        public string email { get; set; }
+        [DisplayName("tipo de usuario")]
+        public int tipoUsuario { get; set; }
+        [DisplayName("pasillo")]
+        public Pasillo pasillo{ get; set; }
+
+    }
+}
